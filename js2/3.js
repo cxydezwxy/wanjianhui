dda03651bafdc8571f3c6f105ff9d7afdb034697
cxydezwxy <1561422996@qq.com>
2018-05-30 23:31:10 +0800

    修改

--- js2/3.js
+++ js2/3.js
@@ -1,10 +1,32 @@
 var data1 = JSON.parse(sessionStorage.getItem('key'));
 console.log(data1);
 var t = 0;
+var q;
 $(document).ready(function () {
     for (i = 0; i < data1.length; i++) {
         t++;
-        $("#panda").append("<div class=round>" + data1[i] + "<p class=gry >" + t + "号" + "</p>" + "</div>");
+        $("#panda").append("<div class=round>" + data1[i] + "<p class=gry >" + t + "号" + "</p>" + "<img src=10.png class=niff>" + "</div>");
     }
+    // $(".round").hover(function () {
+    //     $(this).css({
+    //         "border": "3px solid red"
+    //     });
+    // }, function () {
+    //     $(this).css({
+    //         "border": "3px solid #fff"
+    //     });
+    // });
+    $(".round").click(function () {
+        for (i = 0; i < data1.length; i++) {
+            console.log(i)  
+            $(".backl").css({"background-color": "#f5c97b"});
+        }
+        $(".niff").hide();
+        q = $(".round").index($(this));
+        console.log(this);
+        console.log(q);
+        $(".niff").eq(q).show();
+        $(".killnum").eq(q).addClass("backl");
+        console.log(data1.length);    
+    });
 });
-console.log(data1.length);
--- js2/5.js
+++ js2/5.js
@@ -1,59 +1,59 @@
 var $fsm = new StateMachine({
-    init: 'none',
+    init: '0',
     transitions: [{
             name: 'work1',
-            from: 'none',
-            to: 'time1'
+            from: '0',
+            to: '1',
         },
         {
             name: 'work2',
-            from: 'time1',
-            to: 'time2'
+            from: '1',
+            to: '2',
         },
         {
             name: 'work3',
-            form: 'time2',
-            to: 'time3'
+            from: '2',
+            to: '3',
         },
         {
             name: 'work4',
-            form: 'time3',
-            to: 'time4'
-        }
+            from: '3',
+            to: '4',
+        },
     ],
-    methods:{
-        onWork1:function(){
+    methods: {
+        onWork1: function () {
             $(".fren").addClass("die");
             $(".left").addClass("ded");
         },
-        onWork2:function(){
+        onWork2: function () {
             $(".fren1").addClass("die");
             $(".left1").addClass("ded");
         },
-        onWork3:function(){
+        onWork3: function () {
             $(".fren2").addClass("die");
             $(".left2").addClass("ded");
         },
-        onWork4:function(){
+        onWork4: function () {
             $(".fren3").addClass("die");
             $(".left3").addClass("ded");
         }
     }
-    
 });
-$(".fren").click(function(){
-    $fsm.onWork1();
-    alert("进入杀人环节")
-})
-$(".fren1").click(function(){
-    $fsm.onWork2();
+$(".fren").click(function () {
+    $fsm.work1();
+    alert("请杀手杀人")
+    window.location.href = "sharen.html";
+});
+$(".fren1").click(function () {
+    $fsm.work2();
     alert("请亡灵发表遗言")
-})
-$(".fren2").click(function(){
-    $fsm.onWork3();
+});
+$(".fren2").click(function () {
+    $fsm.work3();
     alert("请玩家依次发言")
-})
-$(".fren3").click(function(){
-    $fsm.onWork4();
+});
+$(".fren3").click(function () {
+    $fsm.work4();
     alert("请全民投票")
-})
+});
